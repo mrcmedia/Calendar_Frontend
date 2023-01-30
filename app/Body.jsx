13163a2097a21abb5b27c8f1d0a7c961931fac56
mrcmedia <mediaunitmrc@gmail.com>
2023-01-30 15:35:24 +0530

    sixth commit

--- app/Body.jsx
+++ app/Body.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Hamburger from 'hamburger-react';
 import React, { useEffect, useState } from 'react'
 import Clock from 'react-live-clock';
+import {useForm} from 'react-hook-form';
 
 const Body = () => {
   const [date] = useState(new Date().toLocaleString("en-LK", {timeZone: "Asia/Colombo", dateStyle:'long'}));
@@ -11,6 +12,19 @@ const Body = () => {
   const [userName, setUserName] = useState('');
   const [IsBig , setIsBig] = useState(window.innerWidth >= 768);
   const [Width, setWidth] = useState(0);
+
+  const EventForm = useForm();
+  const BirthdayForm = useForm();
+
+  const handleEventSubmit = (event) => {
+    console.log(event);
+    document.getElementById('eventForms').reset();
+  }
+
+  const handleBirthdaySubmit = (event) => {
+    console.log(event);
+    document.getElementById('birthdayForms').reset();
+  }
 
   useEffect(() => {
     axios.get('/api/calendar/auth/get-user').then((response)=> {
@@ -88,62 +102,68 @@ const Body = () => {
         </section>
         <section className='py-5 w-full shadow-md bg-slate-50'>
           <h1 className='text-center text-md'>Add Event</h1>
-          <form className='w-[90%] mx-auto'>
+          <form id='eventForms' onSubmit={EventForm.handleSubmit(handleEventSubmit)} className='w-[90%] mx-auto'>
             <label className='text-sm' htmlFor="summary">Summary :</label>
-            <input placeholder='Summary' type="text" className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 mb-3 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='summary' />
+            <input name='summary' {...EventForm.register('summary', {required:true})} placeholder='Summary' type="text" className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 focus:shadow-sm focus:outline-blue-500 mb-3 outline-1 p-1 px-3 text-sm' id='summary' />
+            {EventForm.formState.errors.summary && <p className='text-sm w-full text-red-600 mb-3'>summary required! (this is like a title)</p>}
 
-            <label className='bg-white text-sm' htmlFor="description">Description :</label>
-            <textarea placeholder='Description' className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mb-3 mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='description' />
+            <label className=' bg-white text-sm' htmlFor="description">Description :</label>
+            <textarea {...EventForm.register('description', {required:true})} name='description' placeholder='Description' className='bg-white outline-slate-200 mb-3 outline rounded-sm w-full transition-shadow mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='description' />
+            {EventForm.formState.errors.description && <p className='text-sm w-full text-red-600 mb-3'>Description about event required!</p>}
 
             <label className='bg-white text-sm' htmlFor="startDate">Start Date & Time : </label>
-            <input defaultValue={'2023-01-01T12:00:30'} type="datetime-local" className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 mb-3 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='startDate' />
+            <input {...EventForm.register('startDate', {required:true})}  name='startDate' defaultValue={'2023-01-01T12:00:30'} type="datetime-local" className='bg-white mb-3 outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='startDate' />
+            {EventForm.formState.errors.startDate && <p className='text-sm w-full text-red-600 mb-3'>Starting Date required!</p>}
 
             <label className='bg-white text-sm' htmlFor="endDate">End Date & Time : </label>
-            <input defaultValue={'2023-01-01T12:00:30'} type="datetime-local" className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 mb-3 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='endDate' />
+            <input {...EventForm.register('endDate', {required:true})} name='endDate' defaultValue={'2023-01-01T12:00:30'} type="datetime-local" className='bg-white mb-3 outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='endDate' />
+            {EventForm.formState.errors.endDate && <p className='text-sm w-full text-red-600 mb-3'>Ending Date required!</p>}
 
-            <label className='bg-white text-sm' htmlFor="endDate">Location :</label>
-            <select className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 mb-3 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id="">
-              <option value="">Homagama , Sri Lanka</option>
-              <option value="">Sri Lanka</option>
-              <option value="">Kalutara , Sri Lanka</option>
-              <option value="">Gampaha , Sri Lanka</option>
-              <option value="">Matara , Sri Lanka</option>
-              <option value="">Galle , Sri Lanka</option>
-              <option value="">Hambanthota , Sri Lanka</option>
-              <option value="">Trincomale , Sri Lanka</option>
-              <option value="">Madakalapuwa , Sri Lanka</option>
-              <option value="">Jaffna , Sri Lanka</option>
-              <option value="">Mulathiv , Sri Lanka</option>
-              <option value="">Puththalama , Sri Lanka</option>
-              <option value="">Anuradhapura , Sri Lanka</option>
-              <option value="">Polonnaruwa , Sri Lanka</option>
-              <option value="">Kandy , Sri Lanka</option>
-              <option value="">Nuwara Eliya , Sri Lanka</option>
-              <option value="">Badulla , Sri Lanka</option>
-              <option value="">Badulla , Sri Lanka</option>
-              <option value="">Badulla , Sri Lanka</option>
-              <option value="">Halawatha , Sri Lanka</option>
-              <option value="">Mannarama , Sri Lanka</option>
-              <option value="">Matale , Sri Lanka</option>
-              <option value="">Dambulla , Sri Lanka</option>
-              <option value="">Katharagama , Sri Lanka</option>
+            <label className='bg-white text-sm' htmlFor="location">Location :</label>
+            <select {...EventForm.register('location', {required:true})} name='location' className='bg-white outline-slate-200 mb-3 outline rounded-sm w-full transition-shadow mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id="location">
+              <option value="Homagama , Sri Lanka">Homagama , Sri Lanka</option>
+              <option value="Sri Lanka">Sri Lanka</option>
+              <option value="Kalutara , Sri Lanka">Kalutara , Sri Lanka</option>
+              <option value="Gampaha , Sri Lanka">Gampaha , Sri Lanka</option>
+              <option value="Matara , Sri Lanka">Matara , Sri Lanka</option>
+              <option value="Galle , Sri Lanka">Galle , Sri Lanka</option>
+              <option value="Hambanthota , Sri Lanka">Hambanthota , Sri Lanka</option>
+              <option value="Trincomale , Sri Lanka">Trincomale , Sri Lanka</option>
+              <option value="Madakalapuwa , Sri Lanka">Madakalapuwa , Sri Lanka</option>
+              <option value="Jaffna , Sri Lanka">Jaffna , Sri Lanka</option>
+              <option value="Mulathiv , Sri Lanka">Mulathiv , Sri Lanka</option>
+              <option value="Puththalama , Sri Lanka">Puththalama , Sri Lanka</option>
+              <option value="Anuradhapura , Sri Lanka">Anuradhapura , Sri Lanka</option>
+              <option value="Polonnaruwa , Sri Lanka">Polonnaruwa , Sri Lanka</option>
+              <option value="Kandy , Sri Lanka">Kandy , Sri Lanka</option>
+              <option value="Nuwara Eliya , Sri">Nuwara Eliya , Sri Lanka</option>
+              <option value="Badulla , Sri Lanka">Badulla , Sri Lanka</option>
+              <option value="Badulla , Sri Lanka">Badulla , Sri Lanka</option>
+              <option value="Badulla , Sri Lanka">Badulla , Sri Lanka</option>
+              <option value="Halawatha , Sri Lanka">Halawatha , Sri Lanka</option>
+              <option value="Mannarama , Sri Lanka">Mannarama , Sri Lanka</option>
+              <option value="Matale , Sri Lanka">Matale , Sri Lanka</option>
+              <option value="Dambulla , Sri Lanka">Dambulla , Sri Lanka</option>
+              <option value="Katharagama , Sri Lanka">Katharagama , Sri Lanka</option>
             </select>
+            {EventForm.formState.errors.location && <p className='text-sm w-full text-red-600 mb-3'>Location required!</p>}
             <button className=' w-full p-2 bg-blue-600 text-white rounded-sm hover:bg-blue-500 active:bg-blue-700'>Add Event</button>
           </form>
         </section>
         <p className='bg-white text-center mt-3 text-slate-400'> ------ OR ------ </p>
         <h1 className='bg-white text-center text-md mt-5'>Add Birthday</h1>
-        <form className='bg-white w-[90%] mx-auto my-5'>
-            <label className='bg-white text-sm' htmlFor="summary">Bidthday Boy / Girl Name :</label>
-            <input type="text" placeholder='Happy Birthday' className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 mb-3 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='summary' />
+        <form id='birthdayForms' onSubmit={BirthdayForm.handleSubmit(handleBirthdaySubmit)} className='bg-white w-[90%] mx-auto my-5'>
+            <label className='bg-white text-sm' htmlFor="birthdayPerson">Bidthday Boy / Girl Name :</label>
+            <input {...BirthdayForm.register('birthdayPerson', {required:true})} name='birthdayPerson' type="text" placeholder='Happy Birthday' className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mt-1 mb-3 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='birthdayPerson' />
+            {BirthdayForm.formState.errors.birthdayPerson && <p className='text-sm w-full text-red-600 mb-3'>Person Name / Post required!</p>}
 
-            <label className='bg-white text-sm' htmlFor="description">Who is he/her :</label>
-            <textarea placeholder='teacher security' className='bg-white outline-slate-200 outline rounded-sm w-full transition-shadow mb-3 mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='description' />
+            <label className='bg-white text-sm' htmlFor="birthdaydescription">Who is he/her :</label>
+            <textarea {...BirthdayForm.register('birthdaydescription', {required:true})} name='birthdaydescription' placeholder='teacher security' className='bg-white outline-slate-200 outline mb-3 rounded-sm w-full transition-shadow mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='birthdaydescription' />
+            {BirthdayForm.formState.errors.birthdaydescription && <p className='text-sm w-full text-red-600 mb-3'>Some description required!</p>}
 
-            <label className='bg-white text-sm' htmlFor="startDate">Date Of Birth : </label>
-            <input type="datetime-local" defaultValue={'2023-01-01T12:00:30'} className=' outline-slate-200 bg-white outline rounded-sm w-full transition-shadow mt-1 mb-3 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='startDate' />
-
-
+            <label className='bg-white text-sm' htmlFor="dateofbirth">Date Of Birth : </label>
+            <input {...BirthdayForm.register('dateofbirth', {required:true})}  name='dateofbirth' type="datetime-local" defaultValue={'2023-01-01T12:00:30'} className=' outline-slate-200 mb-3 bg-white outline rounded-sm w-full transition-shadow mt-1 focus:shadow-sm focus:outline-blue-500 outline-1 p-1 px-3 text-sm' id='dateofbirth' />
+            {BirthdayForm.formState.errors.dateofbirth && <p className='text-sm w-full text-red-600 mb-3'>Date of Birth required!</p>}
             <button className='w-full p-2 bg-blue-600 text-white rounded-sm hover:bg-blue-500 active:bg-blue-700'>Add Birthday</button>
           </form>
       </div>
